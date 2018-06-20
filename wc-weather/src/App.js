4319685f39b7ebc33027bd24df0c0ba5cd3d79f8
maidazuha <projectacc.zm@gmail.com>
2018-06-20 17:16:04 +0100

    working weather app

--- wc-weather/src/App.js
+++ wc-weather/src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import axios from "axios";
 import shortid from "shortid";
 import City from "./City";
-import Manager from "./Manager";
 import football from "./data/ball.png";
-import sky from "./data/sky.png";
+import { Doughnut } from "react-chartjs-2";
+import "bulma/css/bulma.css";
+
 class App extends Component {
   state = {
     data: []
@@ -13,7 +14,7 @@ class App extends Component {
   componentDidMount() {
     axios
       .get(
-        `http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743&units=metric&APPID=7373321721ec4fc8ecfb83165c626c60`
+        `http://api.openweathermap.org/data/2.5/group?id=2643741,2464470,2800866,3703443,524901&units=metric&APPID=7373321721ec4fc8ecfb83165c626c60`
       )
       .then(({ data: { list } }) => {
         this.setState({
@@ -27,46 +28,50 @@ class App extends Component {
   render() {
     const { data } = this.state;
     const formattedData = this.formatData(data);
+    console.log(formattedData);
     return (
       <div className="App">
         <header className="App-header">
-<<<<<<< HEAD
           <h1 className="headername">
-            <img src={football} className="App-logo" />World Cup Weather App
-            <img src={football} className="App-logo" />
-=======
-          <h1>
             <img src={football} alt="A football" className="App-logo" />World
             Cup Weather App
             <img src={football} alt="A football" className="App-logo" />
->>>>>>> 30519215ce341b5590ce4fbb1feb2861ad778e93
           </h1>
 
           <select className="options" onChange={this.handleSelect}>
-            <option value={""}>Choose a city</option>
+            <option selected="" value={""}>
+              Choose a city
+            </option>
             {Object.keys(formattedData).map(city => {
               return (
-                <option value={city} key={shortid.generate()}>
+                <option
+                  selected="selected"
+                  value={city}
+                  key={shortid.generate()}
+                >
                   {city}
                 </option>
               );
             })}
           </select>
         </header>
+
         {this.state.city && (
           <City cities={formattedData} currentCity={this.state.city} />
         )}
-        {this.state.city && (
-          <Manager cities={formattedData} currentCity={this.state.city} />
-        )}
-        {/* <img src={sky} /> */}
       </div>
     );
   }
 
   formatData = data => {
     return data.reduce((acc, datum) => {
-      acc[datum.name] = datum;
+      if (datum.name === "City of London") {
+        acc["London"] = datum;
+      } else if (datum.name === "Panama") {
+        acc["Panama City"] = datum;
+      } else {
+        acc[datum.name] = datum;
+      }
       return acc;
     }, {});
   };
--- wc-weather/src/City.js
+++ wc-weather/src/City.js
@@ -1,51 +1,79 @@
 import React from "react";
+import { format } from "url";
 
-const CityWeather = ({ cities, currentCity }) => {
-  return (
-    <div>
-      <h1 className="App-city">
-        Weather in {currentCity}, {cities[currentCity].sys.country}{" "}
-      </h1>
-      <h3>{cities[currentCity].main.temp}°C</h3>
-      <h3>{cities[currentCity].weather[0].description}</h3>
-      <table className="App" align="center">
-        <tbody>
-          <tr>
-            <td>Humidity</td>
-            <td>{cities[currentCity].main.humidity}%</td>
-          </tr>
-          <tr>
-            <td>Max Temp</td>
-            <td>{cities[currentCity].main.temp_max}</td>
-          </tr>
-          <tr>
-            <td>Min Temp</td>
-            <td>{cities[currentCity].main.temp_min}</td>
-          </tr>
-          <tr>
-            <td>Sunrise</td>
-            <td>{cities[currentCity].sys.sunrise}</td>
-          </tr>
-          <tr>
-            <td>Sunset</td>
-            <td>{cities[currentCity].sys.sunset}</td>
-          </tr>
-          <tr>
-            <td>Wind</td>
-            <td>{cities[currentCity].wind.speed}</td>
-          </tr>
-          <tr>
-            <td>Icon</td>
-            <td>{cities[currentCity].weather[0].icon}</td>
-          </tr>
-          <tr>
-            <td>Main Forecast</td>
-            <td>{cities[currentCity].weather[0].main}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+class CityWeather extends React.Component {
+  render() {
+    const cities = this.props.cities;
+    const currentCity = this.props.currentCity;
+
+    const unixSunriseTimeStamp = cities[currentCity].sys.sunrise;
+    const date = new Date(unixSunriseTimeStamp * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedSunrise =
+      String(minutes).length === 2
+        ? `${hours}:${minutes}`
+        : `${hours}:${minutes}0`;
+
+    const unixSunsetTimeStamp = cities[currentCity].sys.sunset;
+    const sunSetdate = new Date(unixSunsetTimeStamp * 1000);
+    const sunSethours = sunSetdate.getHours();
+    const sunSetminutes = sunSetdate.getMinutes();
+    const formattedSunset = `${sunSethours}:${sunSetminutes}`;
+
+    return (
+      <div>
+        <h1 className="App-city">
+          Weather in {currentCity}, {cities[currentCity].sys.country}{" "}
+        </h1>
+        <h3>{cities[currentCity].main.temp}°C</h3>
+
+        <table class="table" align="center">
+          <tbody>
+            <tr>
+              <td>Max Temp</td>
+              <td>{cities[currentCity].main.temp_max} °C</td>
+            </tr>
+            <tr>
+              <td>Min Temp</td>
+              <td>{cities[currentCity].main.temp_min}°C</td>
+            </tr>
+            <tr>
+              <td>Sunrise</td>
+              <td>{formattedSunrise}</td>
+            </tr>
+            <tr>
+              <td>Sunset</td>
+              <td>{formattedSunset}</td>
+            </tr>
+            <tr>
+              <td>Main Forecast</td>
+              <td>{this.emojiWeather(cities[currentCity].weather[0].main)} </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  emojiWeather = description => {
+    const emojiLookUp = {
+      rain: "🌧️",
+      cloud: "☁️",
+      clear: "☀️"
+    };
+    const lowerCase = description.toLowerCase();
+
+    if (lowerCase.includes("rain")) {
+      return emojiLookUp.rain;
+    } else if (lowerCase.includes("clouds")) {
+      return emojiLookUp.cloud;
+    } else if (lowerCase.includes("clear")) {
+      return emojiLookUp.clear;
+    } else {
+      return description;
+    }
+  };
+}
 
 export default CityWeather;
